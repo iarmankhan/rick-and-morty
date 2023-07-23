e48a881c39e40fbd0429ee1eff27e13a7dff54a3
Arman Khan <work.armankhan@gmail.com>
2023-07-23 12:46:49 +0530

    :construction: setting up apollo client

---
+++ src/components/apollo-provider.tsx
@@ -0,0 +1,24 @@
+import {PropsWithChildren, useMemo} from 'react';
+import {ApolloProvider as BaseApolloProvider} from '@apollo/client';
+import {createApolloClient} from "../graphql/client.ts";
+
+
+export interface ApolloProviderProps extends PropsWithChildren {
+  urlBase: string
+}
+
+export function ApolloProvider(props: ApolloProviderProps) {
+
+  const client = useMemo(() => {
+    return createApolloClient({
+      urlBase: props.urlBase,
+    })
+  }, []);
+
+  return (
+    <BaseApolloProvider client={client}>
+      {props.children}
+    </BaseApolloProvider>
+  )
+}
+
---
+++ src/graphql/client.ts
@@ -0,0 +1,17 @@
+import {ApolloClient, InMemoryCache} from '@apollo/client';
+
+export const apolloCache = new InMemoryCache();
+
+export interface CreateApolloClientConfig {
+  urlBase: string;
+}
+
+export function createApolloClient(cfg: CreateApolloClientConfig) {
+  const { urlBase } = cfg;
+
+
+  return new ApolloClient({
+    uri: `${urlBase}/graphql`,
+    cache: apolloCache,
+  });
+}
